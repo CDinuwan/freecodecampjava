7c2f7ab35eff4ddec50e2d42ccdb75da6390a412
Chanuka Dinuwan <chanukadinuwan35@gmail.com>
2022-08-18 13:05:09 +0530

    Updated

--- src/main/java/com/javatraining/l1/Person.java
+++ src/main/java/com/javatraining/l1/Person.java
@@ -5,11 +5,34 @@ import com.javatraining.l2.Name;
 public class Person {
 
     private Name personName;
+    private static int personCounter;
+
+    public Person() {
+        personCounter++;
+        /*
+        empty on purpose --> default constructor
+         */
+
+        //single line comment
+    }
+
+    public Person(Name personName) {
+        this.personName = personName;
+    }
+
     public String helloWorld() {
         return "Hello World!";
     }
 
-    public Name name() {
+    public Name returnName() {
         return personName;
+    }
+
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+
+    public static int numberOfPersons() {
+        return personCounter;
     }
 }
--- src/test/java/com/javatraining/l1/PersonTest.java
+++ src/test/java/com/javatraining/l1/PersonTest.java
@@ -1,6 +1,9 @@
 package com.javatraining.l1;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonTest {
@@ -9,5 +12,70 @@ public class PersonTest {
     public void shouldReturnHelloWorld() {
         Person marcus = new Person();
         assertEquals("Hello World!", marcus.helloWorld());
+    }
+
+    @Test
+    public void shouldReturnHelloName() {
+        Person person = new Person();
+        assertEquals("Hello Name", person.hello("Name"));
+    }
+
+    @Test
+    public void shouldReturnNumberOfPersons() {
+
+        for (int i = 0; i < 5; i++) {
+            Person person = new Person();
+        }
+
+        assertEquals(5, Person.numberOfPersons());
+    }
+
+    @Test
+    public void demonstrateBoolean() {
+        boolean monday = false;
+        boolean raining = true;
+        boolean areEqual = monday != raining;
+
+        if (isItMonday() && isItRaining()) {
+            drinkBeer();
+        } else if (isItMonday() || isItRaining()) {
+            drinkWater();
+        } else {
+            drinkMilk();
+        }
+    }
+
+    @Test
+    public void demonstrateBooleanWithInt() {
+        int i = 5;
+        int j = 7;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please Enter Your Number Here: ");
+//        int j = scanner.nextInt();
+
+        if (i == j) {
+            j = 10;
+        } else if (i == j) {
+            i = 8;
+        } else {
+            j++;
+        }
+    }
+
+    private void drinkMilk() {
+    }
+
+    private void drinkWater() {
+    }
+
+    private void drinkBeer() {
+    }
+
+    private boolean isItRaining() {
+        return true;
+    }
+
+    private boolean isItMonday() {
+        return true;
     }
 }
